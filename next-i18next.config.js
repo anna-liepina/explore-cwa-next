4ba7aa4028888948c8e897dea2f5bb297cd5074d
Anna Liepina <anna.liepina@yahoo.co.uk>
2024-01-15 17:43:50 +0000

    fix warning

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -3,8 +3,8 @@ const path = require('path');
 module.exports = {
   i18n: {
     defaultLocale: 'en-US',
-    locales: ['en-US', 'de-DE'],
+    locales: ['en-US'],
     localeDetection: false,
-    localePath: path.resolve('./public/locales'),
   },
+  localePath: path.resolve('./public/locales'),
 };
